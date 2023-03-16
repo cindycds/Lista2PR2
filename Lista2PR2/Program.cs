e1de925fc36acd2ac586706fef9b6aafb89151ac
cindycds <cindy.silva@aluno.ifsp.edu.br>
2023-03-16 11:08:39 -0300

    exerciocio6pronto

--- Lista2PR2/Program.cs
+++ Lista2PR2/Program.cs
@@ -30,13 +30,13 @@ namespace Lista2PR2
 
 
             //EXERCICIO2
-            //float anoatual, anodenascimento, res;
+            //float proxelei, anodenascimento, res;
 
-            //Console.WriteLine("Digite o ano atual em que você esta: ");
-            //anoatual = float.Parse(Console.ReadLine());
+            //Console.WriteLine("Digite o ano da proxima eleiçao: ");
+            //proxelei = float.Parse(Console.ReadLine());
             //Console.WriteLine("Digite o ano em que você nasceu: " );
             //anodenascimento = float.Parse(Console.ReadLine());
-            //res = anoatual * anodenascimento;
+            //res = proxelei * anodenascimento;
             //if (res < 16)
             //    Console.WriteLine("essa pessoa não pode votar.");
             //else
@@ -44,20 +44,101 @@ namespace Lista2PR2
 
 
             //EXERCIOCIO3
-            float l1, l2, l3;
-            Console.WriteLine("Digite o lado 1 do triangulo");
-            l1 = float.Parse(Console.ReadLine());
-            Console.WriteLine("Digite o lado 2 do triangulo");
-            l2 = float.Parse(Console.ReadLine());
-            Console.WriteLine("Digite o lado 3 do triangulo");
-            l3 = float.Parse(Console.ReadLine());
+            //float l1, l2, l3;
+            //Console.WriteLine("Digite o lado 1 do triangulo");
+            //l1 = float.Parse(Console.ReadLine());
+            //Console.WriteLine("Digite o lado 2 do triangulo");
+            //l2 = float.Parse(Console.ReadLine());
+            //Console.WriteLine("Digite o lado 3 do triangulo");
+            //l3 = float.Parse(Console.ReadLine());
 
-            if (l1 == l2 && l2 == l3 && l1 == l3)
-                Console.WriteLine("esse triangulo é equilatero");
-            else if (l1 != l2 && l2 != l3 && l1 != l3)
-                Console.WriteLine("esse triangulo é escaleno");
-            else
-                Console.WriteLine("Seu triangulo é isósceles");
+            //if (l1 == l2 && l2 == l3 && l1 == l3)
+            //    Console.WriteLine("esse triangulo é equilatero");
+            //else if (l1 != l2 && l2 != l3 && l1 != l3)
+            //    Console.WriteLine("esse triangulo é escaleno");
+            //else
+            //    Console.WriteLine("Seu triangulo é isósceles");
+
+
+            //EXERCIOCIO4
+
+
+            //float a, b, c,res;
+            //double raiz, x1, x2;
+            //Console.WriteLine("vamos determinar as raizes de uma equaçao de 2º");
+            //Console.WriteLine("de um valor para A:");
+            //a = float.Parse(Console.ReadLine());
+            //Console.WriteLine("de um valor para B:");
+            //b = float.Parse(Console.ReadLine());
+            //Console.WriteLine("de um valor para C:");
+            //c = float.Parse(Console.ReadLine());
+
+            //res = (b * b) - 4 * a * c;
+
+            //raiz= Math.Sqrt(res1);
+
+            //x1 = (-b + raiz) / 2f * a;
+            //x2= (-b - raiz) / 2f * a;
+
+            //Console.WriteLine("o valor de x1 é:" + x1);
+            //Console.WriteLine("o valor de x2 é:" + x2);
+
+
+
+
+            //Exerciocio5
+
+            //float num, res3, res5;
+
+            //Console.WriteLine("Digite um numero para saber se ele é divisivel por 3 : ");
+            //num = int.Parse(Console.ReadLine());
+
+            //res3 = num % 3f;
+            //res5 = num % 5f;
+
+            //if ((res3 == 0) && (res5 == 0))
+
+            //Console.WriteLine("O numero é divisivel por 3 e 5");
+
+            //else if (res3 == 0)
+            //    Console.WriteLine(" O numero é divisivel apenas por 3");
+
+            //else if (res5 == 0)
+            //    Console.WriteLine(" O numero é divisivel apenas por 5");
+
+            //else
+            //    Console.WriteLine("O numeronao é divisivel por nenhum dos doids numeros");
+
+
+
+
+            //EXERCICIO 6
+
+            //float a1, a2, a3;
+
+            //Console.WriteLine(" Digite o primeiro angulo:");
+            //a1= float.Parse(Console.ReadLine());
+            //Console.WriteLine(" Digite o primeiro angulo:");
+            //a2= float.Parse(Console.ReadLine());
+            //Console.WriteLine(" Digite o primeiro angulo:");
+            //a3= float.Parse(Console.ReadLine());
+
+            //if (a1 == 90 || a2 == 90 || a3 == 90)
+            //    Console.WriteLine("seu triangulo é retangulo");
+
+            //else if (a1 < 90 && a2 < 90 && a3 < 90)
+            //    Console.WriteLine(" Seu angulo é Obtusângulo");
+
+            //else
+            //    Console.WriteLine("Seu angulo é Acutângulo:");
+
+
+
+
+            //EXERCICIO7
+
+
+
             Console.ReadKey();
         }
     }
