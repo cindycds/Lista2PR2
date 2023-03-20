b158381ab6599519dde4c7d734af36c0a1f585a5
cindycds <cindy.silva@aluno.ifsp.edu.br>
2023-03-20 15:36:03 -0300

    terminei

--- Lista2PR2/Program.cs
+++ Lista2PR2/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -136,6 +138,155 @@ namespace Lista2PR2
 
 
             //EXERCICIO7
+
+            //int a, b, c, x;
+
+            //Console.WriteLine("Entre com o valor de A: ");
+            //a = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Entre com o valor de B: ");
+            //b = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Entre com o valor de C: ");
+            //c = int.Parse(Console.ReadLine());
+
+
+            //if (b < a)
+            //{
+            //    x = a;
+            //    a = b;
+            //    b = x;
+            //}
+
+            //if (c < a)
+            //{
+            //    x = a;
+            //    a = c;
+            //    c = x;
+            //}
+
+            //if (c < b)
+            //{
+            //    x = b;
+            //    b = c;
+            //    c = x;
+            //}
+
+            //Console.WriteLine("primeiro valor " + a + " Segundo valor " + b + " Terceiro valor " + c);
+
+
+
+
+
+
+
+            //EXERCICIO8
+            //float ano, res;
+
+            //Console.WriteLine("Digite o ano desejado para saber se ele sera bissexto");
+            //ano= float.Parse(Console.ReadLine());
+
+            //res = ano % 4;
+
+            //if (res == 0)
+            //    Console.WriteLine("esse ano é bissexto");
+            //else
+            //    Console.WriteLine("Esse ano nao é bissexto");
+
+
+
+
+
+
+            //EXERCICIO9
+            //double num1, num2;
+
+
+            //int operacao= 0;
+
+
+
+            //Console.WriteLine("Digite o primeiro numero");
+
+            //num1 = double.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Digite o segundo numero");
+
+            //num2 = double.Parse(Console.ReadLine());
+
+
+
+
+
+
+
+            //Console.WriteLine("Para somar digite 1");
+
+            //Console.WriteLine("Para subtrair digite 2");
+
+            //Console.WriteLine("Para dividir digite 3");
+
+            //Console.WriteLine("Para multiplicar digite 4");
+
+            //Console.WriteLine("Para sair digite 5");
+
+            //operacao = int.Parse(Console.ReadLine());
+
+            //if (operacao == 1)
+
+            //{
+
+            //    Console.WriteLine("Soma = {0}", num1 + num2);
+
+            //}
+
+            //if (operacao == 2)
+
+            //{
+
+            //    Console.WriteLine("Subtração = {0}", num1 - num2);
+
+            //}
+
+            //if (operacao == 3)
+
+            //{
+
+            //    Console.WriteLine("Divisão = {0}", num1 / num2);
+
+            //}
+
+            //if (operacao == 4)
+
+            //{
+
+            //    Console.WriteLine("Multiplicação = {0}", num1 * num2);
+
+            //}
+
+
+
+
+            //EXERCICIO10
+
+            //É importante lembrar que a classe Random utiliza um algoritmo para gerar números aleatórios baseados em um valor inicial,
+            //chamado de semente.Se a semente não for alterada, os números gerados serão sempre os mesmos.Por isso, é recomendado que a semente
+            //seja alterada antes de gerar números aleatórios, para garantir que os resultados sejam realmente aleatórios.Isso pode ser feito passando
+            //um valor inteiro como parâmetro para o construtor da classe Random.Caso não seja necessário controlar a semente, pode - se utilizar o
+            //construtor padrão da classe Random, que utiliza o relógio do sistema como semente.
+            //exemplo: 
+            //Random random = new Random();
+            //int numero = random.Next(1, 11);
+
+            //Random random = new Random();
+            //double numero = random.NextDouble();
+
+
+
+
+
+
+
 
 
 
